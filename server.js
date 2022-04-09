cf0b0730e78b52deb5fb9939f06c576c8c1be842
Giorgos Pasentsis <gpasents@gmail.com>
2022-04-09 12:56:39 +0300

    add functionality

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
 
+app.use(fileUpload());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +13,15 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse",(req,res,next)=>{
+  if(!req.files||Object.keys(req.files).length===0){
+    res.send("Error: No files found");
+  }else{
+    let file = req.files.upfile;
+    console.log(req.files.upfile);
+    res.json({"name":file.name,"type":file.mimetype,"size":file.size});
+  }
+});
 
 
 const port = process.env.PORT || 3000;
